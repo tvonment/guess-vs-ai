42de143ead24bf3910e23e6c28b2f49f7872586c
tvonment <thvome@gmail.com>
2024-11-14 17:46:27 +0100

    additional style changes

--- app/components/GameOver.tsx
+++ app/components/GameOver.tsx
@@ -14,7 +14,7 @@ export default function GameOver({ winner, aiWord, onNavigate }: GameOverProps) 
                 <Image src={`/images/${winner}-icon.png`} width={300} height={300} alt="Logo" className="w-100 h-100 mb-4" />
             </div>
             <div className="flex justify-center mb-4">
-                <h1 className="text-2xl font-bold text-white">{winner === 'ai' ? 'AI' : 'You'} won!</h1>
+                <h1 className="text-2xl font-bold text-white">{winner === 'assistant' ? 'AI' : 'You'} won!</h1>
             </div>
             <div className="flex justify-center mb-4">
                 <p className="text-lg font-semibold text-white">AIs word was: {aiWord}</p>
