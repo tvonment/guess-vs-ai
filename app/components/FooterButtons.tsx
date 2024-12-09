8d4ccbce336c02ec04a5ab142377624aae31f981
tvonment <thvome@gmail.com>
2024-12-09 03:44:40 +0100

    rm FAQ

--- app/components/FooterButtons.tsx
+++ app/components/FooterButtons.tsx
@@ -5,7 +5,6 @@ interface FooterButtonsProps {
 export default function FooterButtons({ openModal }: FooterButtonsProps) {
     return (
         <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
-            <button onClick={() => openModal('FAQ')} className="footer-link m-1">FAQ</button>
             <button onClick={() => openModal('Terms of Use')} className="footer-link m-1">Terms of Use</button>
             <button onClick={() => openModal('Contact')} className="footer-link m-1">Contact</button>
         </div>
